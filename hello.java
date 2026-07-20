1ab1d80df6505a3cddb82e9061f3f4467d83b337
Dhruv Sharma <dhruvsharma192007@gmail.com>
2026-07-20 19:43:44 +0530

    commit

--- hello.java
+++ hello.java
@@ -1,4 +1,8 @@
 class hello{
         public static void main(String[] args){
-                 System.out.println("Hey there, Earth!");
+                 System.out.println("Hey there ,Earth!");
+                 System.out.println("we are in 2026");
+                 
+
+                 
 }}
